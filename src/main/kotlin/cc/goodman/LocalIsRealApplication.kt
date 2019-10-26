f29f0edbae69919c9bbdbdd791d3a44484371cea
ivan_shi <shiyifan00@live.cn>
2019-10-26 19:35:58 +0800

    fix

--- src/main/kotlin/cc/goodman/LocalIsRealApplication.kt
+++ src/main/kotlin/cc/goodman/LocalIsRealApplication.kt
@@ -2,12 +2,14 @@ package cc.goodman
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.ViewResolver
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
+import org.springframework.web.servlet.view.InternalResourceViewResolver
 
 fun main(args: Array<String>) {
     SpringApplication.run(RootContextConfig::class.java, *args)
@@ -23,8 +25,16 @@ open class RootContextConfig {
 @EnableWebMvc
 @ComponentScan(basePackages = ["cc.goodman.controller"])
 open class WebContextConfig : WebMvcConfigurer {
-    override fun configureViewResolvers(registry: ViewResolverRegistry) {
-        registry.jsp("/", ".jsp")
+//    override fun configureViewResolvers(registry: ViewResolverRegistry) {
+//        registry.jsp()
+//    }
+
+    @Bean
+    open fun viewResolver(): ViewResolver {
+        val i = InternalResourceViewResolver();
+        i.setPrefix("/")
+        i.setSuffix(".jsp")
+        return i
     }
 
 //    override fun configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer) {
