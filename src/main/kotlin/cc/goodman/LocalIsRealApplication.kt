88ceedfe571ae12a062e446519b4254be7e1c583
Ivan Shi <shiyifan00@live.cn>
2019-10-28 18:04:17 +0800

    add the thymeleaf but still have some bug

--- src/main/kotlin/cc/goodman/LocalIsRealApplication.kt
+++ src/main/kotlin/cc/goodman/LocalIsRealApplication.kt
@@ -2,9 +2,11 @@ package cc.goodman
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.ViewResolver
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
@@ -12,6 +14,10 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer
 import org.springframework.web.servlet.view.InternalResourceViewResolver
+import org.thymeleaf.spring5.SpringTemplateEngine
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver
+import org.thymeleaf.spring5.view.ThymeleafViewResolver
+import org.thymeleaf.templatemode.TemplateMode
 
 fun main(args: Array<String>) {
     SpringApplication.run(RootContextConfig::class.java, *args)
@@ -26,22 +32,45 @@ open class RootContextConfig {
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = ["cc.goodman.controller"])
-open class WebContextConfig : WebMvcConfigurer {
-
-    @Bean
-    open fun viewResolver(): ViewResolver {
-        val i = InternalResourceViewResolver();
-        i.setPrefix("/")
-        i.setSuffix(".jsp")
-        return i
-    }
-
-    override fun configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer) {
+open class WebContextConfig(private val appContext: ApplicationContext) : WebMvcConfigurer {
+    override fun configureDefaultServletHandling(configurer: DefaultServletHandlerConfigurer): Unit =
         configurer.enable()
-    }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/img/**").addResourceLocations("/img/")
+    }
+
+    @Bean
+    open fun jspViewResolver(): ViewResolver = InternalResourceViewResolver().apply {
+        setPrefix("/")
+        setSuffix(".jsp")
+        order = Int.MAX_VALUE
+    }
+
+    @Bean
+    open fun messageSource(): ReloadableResourceBundleMessageSource = ReloadableResourceBundleMessageSource().apply {
+        setBasename("i18n/label")
+    }
+
+    @Bean
+    open fun thymeleafTemplateResolver(): SpringResourceTemplateResolver = SpringResourceTemplateResolver().apply {
+        setApplicationContext(appContext)
+        prefix = "/template/"
+        suffix = ".html"
+        templateMode = TemplateMode.HTML
+        isCacheable = false
+    }
+
+    @Bean
+    open fun thymeleafTemplateEngine(): SpringTemplateEngine = SpringTemplateEngine().apply {
+        setTemplateResolver(thymeleafTemplateResolver())
+        enableSpringELCompiler = true
+    }
+
+    @Bean
+    open fun thymeleafViewResolver(): ThymeleafViewResolver = ThymeleafViewResolver().apply {
+        templateEngine = thymeleafTemplateEngine()
+        order = 1
     }
 }
 
--- src/main/kotlin/cc/goodman/controller/HomeController.kt
+++ src/main/kotlin/cc/goodman/controller/HomeController.kt
@@ -1,15 +1,18 @@
 package cc.goodman.controller
 
 import org.apache.logging.log4j.LogManager
+import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import java.util.*
 
 @Controller
-class HomeController {
+class HomeController(private val appContext: ApplicationContext) {
     private val logger = LogManager.getLogger()
     @GetMapping("/hello")
     fun hello(): String {
-        logger.info("hello mapping")
-        return "home";
+        logger.info("Hello Hello Hello")
+        logger.info(appContext.getMessage("hello", null, Locale.getDefault()))
+        return "thymeleafHome";
     }
 }
